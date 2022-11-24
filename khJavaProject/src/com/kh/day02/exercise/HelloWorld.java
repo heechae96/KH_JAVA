26ef821db4cf2e492d51256aa403d64343cc7ab0
heechae96 <shinheechae@naver.com>
2022-11-24 10:06:56 +0900

    day02 first commit

---
+++ khJavaProject/src/com/kh/day02/exercise/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.kh.day02.exercise;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		// 주석
+		// -> 실행에 영향을 끼치지 않는다
+		System.out.println("Hello Java");
+	}
+
+}
